5efaaa964b9de53a1cd1687d1ea10196cf5ccc74
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-03-12 13:02:43 +0530

    enums added

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:wordle/enums/keyboard_keys.dart';
 
 class DataSingleton {
   static final DataSingleton _dataSingleton = DataSingleton._internal();
@@ -12,5 +15,25 @@ class DataSingleton {
     return _dataSingleton;
   }
 
-  DataSingleton._internal();
+  DataSingleton._internal() {
+    if (secretWord.isEmpty) {
+      createWord();
+    }
+  }
+  // bool setLetter(KeyboardKeys key) {
+  //   if (KeyboardKeys.enter.name == key.name) {
+  //     return false;
+  //   }
+  // }
+
+  Future<String> createWord() async {
+    final words = (await rootBundle.loadString('assets/words.txt')).split("\n");
+    var now = DateTime.now();
+    var random = Random(now.year * 10000 + now.month * 100 + now.day);
+    var index = random.nextInt(words.length);
+    secretWord = words[index];
+    allWords = words.toSet();
+    return secretWord;
+  }
+
 }
