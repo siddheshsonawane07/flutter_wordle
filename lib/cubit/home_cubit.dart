15c312ed6a6566240a050ebae1731ba70a4cdbe4
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-03-13 17:04:21 +0530

    changes

--- lib/cubit/home_cubit.dart
+++ lib/cubit/home_cubit.dart
@@ -1,53 +1,53 @@
-// import 'package:bloc/bloc.dart';
-// import 'package:equatable/equatable.dart';
-// import 'package:wordle/data/data.dart';
-// import 'package:wordle/enums/keyboard_keys.dart';
-// import 'package:wordle/enums/letter.dart';
-// import 'package:wordle/enums/message_types.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:wordle/data/data.dart';
+import 'package:wordle/enums/keyboard_keys.dart';
+import 'package:wordle/enums/letter.dart';
+import 'package:wordle/enums/message_types.dart';
 
-// part 'home_state.dart';
+part 'home_state.dart';
 
-// class HomeCubit extends Cubit<HomeState> {
-//   HomeCubit() : super(HomeInitial());
-//   DataSingleton data = DataSingleton();
+class HomeCubit extends Cubit<HomeState> {
+  HomeCubit() : super(HomeInitial());
+  DataSingleton data = DataSingleton();
 
-//   void setLetter(KeyboardKeys key) {
-//     if (data.setLetter(key)) {
-//       emit(GridUpdateState());
-//     }
-//   }
+  void setLetter(KeyboardKeys key) {
+    if (data.setLetter(key)) {
+      emit(GridUpdateState());
+    }
+  }
 
-//   void removeLetter() {
-//     data.removeLetter();
-//     emit(GridUpdateState());
-//   }
+  void removeLetter() {
+    data.removeLetter();
+    emit(GridUpdateState());
+  }
 
-//   bool submitWord() {
-//     final state = data.submitWord();
-//     if (state is GridUpdateState ||
-//         state is WinGameState ||
-//         state is LoseGameState) {
-//       emit(state);
-//       if (state is WinGameState) {
-//         emit(GridUpdateState());
-//       }
-//       return true;
-//     }else if(state is SnackBarMessage){
-//       emit(state);
-//       return false;
-//     }
-//     return false;
-//   }
+  bool submitWord() {
+    final state = data.submitWord();
+    if (state is GridUpdateState ||
+        state is WinGameState ||
+        state is LoseGameState) {
+      emit(state);
+      if (state is WinGameState) {
+        emit(GridUpdateState());
+      }
+      return true;
+    }else if(state is SnackBarMessage){
+      emit(state);
+      return false;
+    }
+    return false;
+  }
 
-//   void updateKey(KeyboardKeys key, Letter letterType) {
-//     emit(KeyboardKeyUpdateState(key, letterType));
-//   }
+  void updateKey(KeyboardKeys key, Letter letterType) {
+    emit(KeyboardKeyUpdateState(key, letterType));
+  }
 
-//   Future clearGameArea() async {
-//     data.resetData();
-//     await data.createWord();
-//     emit(HomeInitial());
-//   }
+  Future clearGameArea() async {
+    data.resetData();
+    await data.createWord();
+    emit(HomeInitial());
+  }
 
 
-// }
+}
